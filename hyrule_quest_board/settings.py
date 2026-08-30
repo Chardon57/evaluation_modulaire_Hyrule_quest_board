a87a5c788752f60819b9ab919ad76910c2f5c58d
Jean-Patrick Glad <jpglad@protonmail.com>
2026-08-30 14:39:26 +0200

    Ajout du .env et des bibliothèques nécessaires pour l'utiliser

--- hyrule_quest_board/settings.py
+++ hyrule_quest_board/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_h@(@q#aiu$s68!j92d61+b7q-ck@frppu@=_=2j0_0!t9m0oi'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
